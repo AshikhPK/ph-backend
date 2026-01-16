018b3b2420fae44dcc4a87bc1b252f7f267ce528
AshikhPK <ashikhhari2004@gmail.com>
2026-01-16 14:24:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
+# global storage
 latest_ph = {"value": None}
 
-class PHData(BaseModel):
-    ph: float
-
-@app.post("/api/ph")
-def receive_ph(data: PHData):
-    latest_ph["value"] = data.ph
-    return {"status": "ok"}
+@app.get("/")
+def root():
+    return {"status": "Backend is running"}
 
 @app.get("/api/ph")
 def get_ph():
     return latest_ph
-@app.get("/")
-def root():
-    return {"status": "Backend is running"}
+
 @app.get("/api/ph/update")
 def update_ph(ph: float):
     latest_ph["value"] = ph
-    return {"status": "ok", "value": ph}
-
-
-
+    return {"status": "updated", "value": ph}
