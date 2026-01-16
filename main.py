13ad8ca60e0a2b1b0bb3e806bd64aafd3f0d73a4
AshikhPK <ashikhhari2004@gmail.com>
2026-01-16 13:15:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+latest_ph = {"value": None}
+
+class PHData(BaseModel):
+    ph: float
+
+@app.post("/api/ph")
+def receive_ph(data: PHData):
+    latest_ph["value"] = data.ph
+    return {"status": "ok"}
+
+@app.get("/api/ph")
+def get_ph():
+    return latest_ph
+
